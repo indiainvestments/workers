ca2d1ac4c3281de8fa32e5d6edf3f4c48c112601
lolwatks <86886710+lolwatks@users.noreply.github.com>
2021-07-04 19:11:28 +0530

    Fix strings

--- src/handler.ts
+++ src/handler.ts
@@ -2,9 +2,9 @@ import { updateMetadata } from './metadata'
 
 const redirectHttpCode = 301
 const redirectMap = new Map([
-  ['/discord', '${process.env.DISCORD}'],
-  ['/reddit', '${process.env.REDDIT}'],
-  ['/youtube', '${process.env.YOUTUBE}'],
+  ['/discord', `${process.env.DISCORD}`],
+  ['/reddit', `${process.env.REDDIT}`],
+  ['/youtube', `${process.env.YOUTUBE}`],
 ])
 
 export async function handleRequest(request: Request): Promise<Response> {
