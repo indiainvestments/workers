d797510a39cd1fbe68a62d07cf64b19eca91522d
lolwatks <86886710+lolwatks@users.noreply.github.com>
2021-07-04 20:29:41 +0530

    Fix broken injected variables

--- src/handler.ts
+++ src/handler.ts
@@ -1,22 +1,15 @@
 import { updateMetadata } from './metadata'
 
-declare global {
-  const DISCORD_INVITE: string;
-  const REDDIT_LINK: string;
-  const YOUTUBE_CHANNEL: string;
-}
-
 const redirectHttpCode = 301
 const redirectMap = new Map([
-  ['/discord', DISCORD_INVITE],
-  ['/reddit', REDDIT_LINK],
+  ['/discord', DISCORD_SERVER],
+  ['/reddit', SUBREDDIT_LINK],
   ['/youtube', YOUTUBE_CHANNEL],
 ])
 
 export async function handleRequest(request: Request): Promise<Response> {
   const url = new URL(request.url)
   const { pathname } = url
-
   const targetUrl = redirectMap.get(pathname)
   if (targetUrl) {
     return Response.redirect(targetUrl, redirectHttpCode)
